7202ebceca28e3b6743ff5492eaaee1232e8c4ca
Olivier Arteau <spam_arto_1@hotmail.com>
2011-11-15 19:43:17 -0500

    Fixed bug when having odd number of members. Mini cleanup in the members haml file.

--- js-montreal.rb
+++ js-montreal.rb
@@ -71,6 +71,7 @@ helpers do
 
   # Returns the URL for the gravatar image associated with the email
   def gravaturl(email)
+    email ||= ""
     hash = MD5::md5(email.downcase)
     "http://www.gravatar.com/avatar/#{hash}"
   end
