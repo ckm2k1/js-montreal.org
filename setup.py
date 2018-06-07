20a8e5638337153f95c9ed7fedc030d98d52bada
Guillaume Smaha <guillaume.smaha@gmail.com>
2018-06-07 17:01:21 -0400

    Upgrade to 0.0.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 setup(
     name='borgy-process-agent',
-    version='0.0.6',
+    version='0.0.7',
     description='',
     author='Borygy Team',
     packages=find_packages(exclude=('tests', 'docs')),
