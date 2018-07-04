2925a111e7eb7b9004433e2c3e554c15d2f3796f
Guillaume Smaha <guillaume.smaha@gmail.com>
2018-07-04 16:51:43 -0400

    Upgrade to 1.3.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 setup(
     name='borgy-process-agent',
-    version='1.2.1',
+    version='1.3.0',
     description='',
     author='Borygy Team',
     packages=find_packages(exclude=('tests', 'docs')),
