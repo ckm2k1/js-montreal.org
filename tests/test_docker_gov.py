7c8fd0df0b1cf1af361b26bd2bef70f30c0b9da1
Steve Venzerul <stanislav.venzerul@elementai.com>
2020-02-06 15:14:38 -0500

    docker gov tests

--- tests/test_docker_gov.py
+++ tests/test_docker_gov.py
@@ -1,11 +1,12 @@
 import enum
-from typing import List, Mapping, Callable
-from unittest.mock import patch, Mock
+from typing import List, Mapping, Callable, cast
+from unittest.mock import patch, Mock, MagicMock
 
 from borgy_process_agent_api_server.models import JobSpec, JobsOps
 
+from borgy_process_agent.enums import State
 from borgy_process_agent.runners.docker_gov import DockerGovernor
-from borgy_process_agent.utils import get_now_isoformat
+from borgy_process_agent.utils import get_now_isoformat, ObjDict
 
 
 class DockerStatus(enum.Enum):
@@ -16,7 +17,7 @@ class DockerStatus(enum.Enum):
     exited = 'exited'
 
 
-class MockDockerContainer:
+class DockerContainer:
 
     def __init__(self, data=None, logs=b''):
         self.attrs = {
@@ -32,7 +33,7 @@ class MockDockerContainer:
                 'OOMKilled': False,
                 'Dead': False,
                 'Pid': 0,
-                'ExitCode': 0,
+                'ExitCode': None,
                 'Error': '',
                 'StartedAt': '',
                 'FinishedAt': '',
@@ -62,6 +63,9 @@ class MockDockerContainer:
     def set_state(self, **kwargs):
         self.attrs['State'].update(kwargs)
 
+    def get_state(self, attr):
+        return self.attrs['State'].get(attr)
+
     @property
     def name(self):
         name = self.attrs.get('Name')
@@ -73,18 +77,25 @@ class MockDockerContainer:
     def name(self, val):
         self.attrs['Name'] = val
 
-    def stop(self):
+    def stop(self, timeout=None):
         self._stopped = True
         self.set_state(Status=DockerStatus.exited.value,
                        Running=False,
                        FinishedAt=get_now_isoformat())
+
+    def terminate(self):
+        if not self._stopped:
+            self.set_state(Status=DockerStatus.exited.value,
+                           Running=False,
+                           ExitCode=0,
+                           FinishedAt=get_now_isoformat())
 
     def logs(self, **kwargs):
         return self._logs
 
     def remove(self):
         self._removed = True
-        self.status = DockerStatus.exited.value
+        self.set_state(Status=DockerStatus.exited.value)
 
     def init(self):
         self.set_state(StartedAt='',
@@ -94,7 +105,7 @@ class MockDockerContainer:
                        Paused=False,
                        Restarting=False,
                        Dead=False,
-                       ExitCode=0,
+                       ExitCode=None,
                        Error='')
 
     def start(self):
@@ -112,12 +123,10 @@ class MockDockerContainer:
         return cont
 
 
-class MockDockerIter():
+class DockerIter():
 
     def __init__(self, containers: List[Mapping], init=True):
-        self._containers: List[MockDockerContainer] = [
-            MockDockerContainer(cont) for cont in containers
-        ]
+        self._containers: List[DockerContainer] = [DockerContainer(cont) for cont in containers]
         if init:
             for cont in self._containers:
                 cont.init()
@@ -133,7 +142,7 @@ class MockDockerIter():
         name = kwargs.get('name')
         cont = self.get_cont(name)
         if cont is None:
-            cont = MockDockerContainer.from_spec(kwargs)
+            cont = DockerContainer.from_spec(kwargs)
         cont.start()
         self._containers.append(cont)
         return cont
@@ -145,45 +154,109 @@ class MockDockerIter():
         return iter(self._containers)
 
 
-def empty_ops():
-    return JobsOps(False, submit=[], rerun=[], kill=[])
+def make_ops(submit=None, rerun=None, kill=None, submit_parallel=False):
+    submit = submit or []
+    rerun = rerun or []
+    kill = kill or []
+    return JobsOps(submit_parallel=submit_parallel, submit=submit, rerun=rerun, kill=kill)
+
+
+def mock_from_env():
+    dockeriter = DockerIter([])
+    mm = MagicMock(wraps=dockeriter)
+    mm.__iter__ = dockeriter.__iter__
+    return ObjDict({'containers': mm})
 
 
 class TestDockerGov:
 
-    def test_create_jobs(self, fixture_loader: Callable):
-        docker_iter = MockDockerIter([])
+    def test_basic_path(self, fixture_loader: Callable):
         specs_json = fixture_loader('specs.json')
 
-        with patch('borgy_process_agent.runners.docker_gov.docker.from_env'):
+        with patch('borgy_process_agent.runners.docker_gov.docker.from_env', wraps=mock_from_env):
             gov = DockerGovernor()
             specs = [JobSpec.from_dict(spec) for spec in specs_json['submit']]
             ops = JobsOps(submit=specs, rerun=[], kill=[])
 
-            with patch.object(gov._docker, 'containers', wraps=docker_iter):
-                with patch.multiple(gov._jobs_api,
-                                    v1_jobs_get_with_http_info=Mock(side_effect=[
-                                        (ops, 200, {}),
-                                        (empty_ops(), 200, {}),
-                                        (empty_ops(), 200, {}),
-                                    ]),
-                                    v1_jobs_put=Mock(return_value=None)):
-                    gov._running = True
-                    gov._run_iteration()
-                    assert len(gov._governor_jobs) == 5
+            with patch.multiple(gov._jobs_api,
+                                v1_jobs_get_with_http_info=Mock(side_effect=[
+                                    (ops, 200, {}),
+                                    (make_ops(), 200, {}),
+                                    (make_ops(), 200, {}),
+                                ]),
+                                v1_jobs_put=Mock(return_value=None)):
+                gov._running = True
+                gov._run_iteration()
+                assert len(gov._governor_jobs) == 5
 
-                    for jid, desc in gov._governor_jobs.items():
-                        job = desc['job']
-                        assert job.state == 'RUNNING'
+                for _, desc in gov._governor_jobs.items():
+                    job = desc['job']
+                    assert job.state == State.RUNNING.value
+                    assert len(job.runs) == 1
+                    assert job.runs[-1].started_on == desc['container'].get_state('StartedAt')
 
-                    # No change iteration.
-                    gov._run_iteration()
+                # No change iteration.
+                gov._run_iteration()
 
-                    for cont in docker_iter:
-                        cont.stop()
+                for cont in gov._docker.containers.list():
+                    cont.terminate()
 
-                    gov._run_iteration()
-                    assert len(gov._governor_jobs) == 5
-                    for desc in gov._governor_jobs.values():
-                        job = desc['job']
-                        assert job.state == 'SUCCEEDED'
+                gov._run_iteration()
+                assert len(gov._governor_jobs) == 5
+                for desc in gov._governor_jobs.values():
+                    job = desc['job']
+                    assert job.state == State.SUCCEEDED.value
+                    assert len(job.runs) == 1
+                    assert job.runs[-1].ended_on == desc['container'].get_state('FinishedAt')
+
+    def test_kill_job(self, fixture_loader: Callable):
+        specs_json = fixture_loader('specs.json')
+
+        with patch('borgy_process_agent.runners.docker_gov.docker.from_env', wraps=mock_from_env):
+            gov = DockerGovernor()
+            specs = [JobSpec.from_dict(spec) for spec in specs_json['submit']]
+            ops = JobsOps(submit=specs, rerun=[], kill=[])
+
+            with patch.multiple(gov._jobs_api,
+                                v1_jobs_get_with_http_info=Mock(side_effect=[(ops, 200, {})]),
+                                v1_jobs_put=Mock(return_value=None)):
+                gov._running = True
+                gov._run_iteration()
+
+                assert len(gov._governor_jobs) == 5
+                for _, desc in gov._governor_jobs.items():
+                    job = desc['job']
+                    assert job.state == State.RUNNING.value
+                    assert len(job.runs) == 1
+                    assert job.runs[-1].started_on == desc['container'].get_state('StartedAt')
+                assert gov._jobs_api.v1_jobs_get_with_http_info.call_count == 1
+
+                kill_jid = list(gov._governor_jobs.keys())[0]
+                res = [(make_ops(kill=[kill_jid]), 200, {}), (make_ops(), 200, {})]
+                gov._jobs_api.v1_jobs_get_with_http_info.reset_mock(side_effect=True)
+                gov._jobs_api.v1_jobs_get_with_http_info.side_effect = res
+
+                gov._run_iteration()
+
+                dead_job = gov._governor_jobs[kill_jid]['job']
+                dead_cont = gov._governor_jobs[kill_jid]['container']
+                assert dead_job.state == State.CANCELLED.value
+                assert dead_job.runs[-1].cancelled_on == dead_cont.get_state('FinishedAt')
+                assert dead_job.runs[-1].ended_on == dead_cont.get_state('FinishedAt')
+                assert dead_cont.get_state('Status') == DockerStatus.exited.value
+                assert dead_cont._stopped is True
+
+                for cont in gov._docker.containers.list():
+                    cont.terminate()
+
+                gov._run_iteration()
+                assert len(gov._governor_jobs) == 5
+                for jid, desc in gov._governor_jobs.items():
+                    job = desc['job']
+                    if jid == kill_jid:
+                        assert job.state == State.CANCELLED.value
+                    else:
+                        assert job.state == State.SUCCEEDED.value
+                    assert len(job.runs) == 1
+                    # from pudb import set_trace; set_trace()
+                    assert job.runs[-1].ended_on == desc['container'].get_state('FinishedAt')
